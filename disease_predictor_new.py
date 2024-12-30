c69aa1ad1646068f016c1a56c8481f6716760f3c
Aadi Joshi <toaadijoshi@gmail.com>
2024-12-30 08:54:10 +0530

    made a .py file, updated README

---
+++ disease_predictor_new.py
@@ -0,0 +1,83 @@
+# Importing libraries
+import numpy as np
+import pandas as pd
+from scipy.stats import mode
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import statistics
+
+# Reading the train.csv
+DATA_PATH = "Training.csv"
+data = pd.read_csv(DATA_PATH).dropna(axis = 1)
+
+# Encoding and data preparation
+encoder = LabelEncoder()
+data["prognosis"] = encoder.fit_transform(data["prognosis"])
+
+X = data.iloc[:,:-1]
+y = data.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y, test_size = 0.2, random_state = 24)
+
+def cv_scoring(estimator, X, y):
+    return accuracy_score(y, estimator.predict(X))
+
+# Initialize and train models
+final_svm_model = SVC()
+final_nb_model = GaussianNB()
+final_rf_model = RandomForestClassifier(random_state=18)
+final_svm_model.fit(X, y)
+final_nb_model.fit(X, y)
+final_rf_model.fit(X, y)
+
+# Reading the test data
+test_data = pd.read_csv("Testing.csv").dropna(axis=1)
+test_X = test_data.iloc[:, :-1]
+test_Y = encoder.transform(test_data.iloc[:, -1])
+
+symptoms = X.columns.values
+symptom_index = {}
+for index, value in enumerate(symptoms):
+    symptom = " ".join([i.capitalize() for i in value.split("_")])
+    symptom_index[symptom] = index
+
+data_dict = {
+    "symptom_index": symptom_index,
+    "predictions_classes": encoder.classes_
+}
+
+def predictDisease(symptoms):
+    symptoms = symptoms.split(",")
+    input_data = [0] * len(data_dict["symptom_index"])
+    for symptom in symptoms:
+        index = data_dict["symptom_index"][symptom]
+        input_data[index] = 1
+    
+    input_data = np.array(input_data).reshape(1, -1)
+    input_df = pd.DataFrame(input_data, columns=X.columns)
+    
+    rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_df)[0]]
+    nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_df)[0]]
+    svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_df)[0]]
+    
+    final_prediction = statistics.mode([rf_prediction, nb_prediction, svm_prediction])
+    
+    predictions = {
+        "rf_model_prediction": rf_prediction,
+        "naive_bayes_prediction": nb_prediction,
+        "svm_model_prediction": svm_prediction,
+        "final_prediction": final_prediction
+    }
+    
+    return predictions
+
+# Testing the function
+predictions = predictDisease("Vomiting,Fatigue,Anxiety,Lethargy")
+
+# Only output the final predictions
+for model, prediction in predictions.items():
+    print(f"{model.replace('_', ' ').title()}: {prediction}")
